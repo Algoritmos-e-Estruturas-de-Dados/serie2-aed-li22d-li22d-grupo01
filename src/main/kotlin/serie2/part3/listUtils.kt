d1b2b91af2bf92bf380b48ee41925bc64a74032a
ghosthetoast <2005.goncalosilva@gmail.com>
2025-05-13 19:05:04 +0100

    Exercício 3.1 terminado

--- src/main/kotlin/serie2/part3/listUtils.kt
+++ src/main/kotlin/serie2/part3/listUtils.kt
@@ -7,7 +7,27 @@ class Node<T> (
 }
 
 fun splitEvensAndOdds(list:Node<Int>){
-    TODO()
+
+    var current = list.next // Primeiro nó depois da sentinela
+
+    while (current != list){ // Percorre a lista até encontrar a sentinela
+
+        var nextNode = current!!.next // Armazena o próximo nó
+
+        if (current.value % 2 == 0){ // Verifica se o value do nó atual é par
+
+            //Remove o nó atual e refaz as ligações
+            current.previous!!.next = current.next
+            current.next!!.previous = current.previous
+
+            //Insere o nó logo depois da sentinela
+            current.next = list.next
+            current.previous = list
+            list.next!!.previous = current
+            list.next = current
+        }
+        current = nextNode
+    }
 }
 
 fun <T> intersection(list1: Node<T>, list2: Node<T>, cmp: Comparator<T>): Node<T>? {
